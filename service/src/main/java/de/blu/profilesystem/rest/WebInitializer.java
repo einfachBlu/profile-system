6fe5879242d790e5e62185fcaaec8ace6a00d74e
Bluplayz <bluplayz98@gmail.com>
2021-06-24 10:14:22 +0200

    bug fixes

--- service/src/main/java/de/blu/profilesystem/rest/WebInitializer.java
+++ service/src/main/java/de/blu/profilesystem/rest/WebInitializer.java
@@ -3,10 +3,7 @@ package de.blu.profilesystem.rest;
 import com.google.inject.Inject;
 import com.google.inject.Injector;
 import com.google.inject.Singleton;
-import de.blu.profilesystem.rest.listener.DeleteProfile;
-import de.blu.profilesystem.rest.listener.InsertProfile;
-import de.blu.profilesystem.rest.listener.ShowProfiles;
-import de.blu.profilesystem.rest.listener.UpdateProfile;
+import de.blu.profilesystem.rest.listener.*;
 import spark.Spark;
 
 @Singleton
@@ -21,7 +18,7 @@ public final class WebInitializer {
     Spark.put("/profile", this.injector.getInstance(InsertProfile.class));
     Spark.delete("/profile", this.injector.getInstance(DeleteProfile.class));
     Spark.patch("/profile", this.injector.getInstance(UpdateProfile.class));
-    Spark.patch("/profileloginupdate", this.injector.getInstance(UpdateProfile.class));
+    Spark.patch("/profileloginupdate", this.injector.getInstance(UpdateLoginProfile.class));
 
     Spark.awaitInitialization();
   }
